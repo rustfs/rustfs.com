5d5e363a0b5fc34066e19d38b53bcf122786065d
loverustfs <hello@rustfs.com>
2025-11-28 11:22:47 +0800

    fix

--- components/business/home-differents.tsx
+++ components/business/home-differents.tsx
@@ -32,7 +32,7 @@ export default function HomeDifferents() {return (
               </div>
               <div className="flex items-center gap-2 p-4 md:flex-row-reverse lg:px-8 ">
                 <XIcon className="text-neutral-500" />
-                <span>{'Ensures data sovereignty and compliance with local logging regulations'}</span>
+                <span>{'Data Sovereignty & Local Compliance'}</span>
               </div>
               <div className="flex items-center gap-2 bg-neutral-100 p-4 md:flex-row-reverse lg:px-8 dark:bg-neutral-900">
                 <XIcon className="text-neutral-500" />
